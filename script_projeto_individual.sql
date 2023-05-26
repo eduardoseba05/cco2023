23514576e856825d7990822b11e80cb1ae02c5a5
eduardoseba05 <125585882+eduardoseba05@users.noreply.github.com>
2023-05-26 11:25:03 -0300

    script_projeto_individual.sql

--- script_projeto_individual.sql
+++ script_projeto_individual.sql
@@ -1,7 +1,19 @@
-create database goleiros_sa;
-use goleiros_sa;
+create database goleiros;
+use goleiros;
 
-create table cadastro (idCadastro int primary key, email varchar(30), senha char(8)); 
+create table cadastro (
+idCadastro int primary key auto_increment, 
+email varchar(30), 
+senha char(8)
+)auto_increment = 1; 
 
 
-create table jogos (idJogos int primary key, minutos_de_jogo varchar(3), gols_sofridos varchar (2), fkCadastro int, foreign key(fkCadastro) references cadastro(idCadastro));
+create table jogos (
+idJogos int primary key auto_increment, 
+gols_feitos varchar (2), 
+gols_sofridos varchar (2), 
+fkCadastro int, 
+foreign key(fkCadastro) references cadastro(idCadastro)
+)auto_increment = 1;
+
+insert into jogos values ();
